b01e727ed46e863e1d451d2aeff5fd0602a436d2
ST10132139 <102595139+ST10132139@users.noreply.github.com>
2024-11-22 12:29:42 +0200

    Change home page to default to the login page

--- Controllers/HRController.cs
+++ Controllers/HRController.cs
@@ -1,6 +1,34 @@
-﻿namespace ContractMonthlyClaimSystem4.Controllers
+﻿using ContractMonthlyClaimSystem4.Data;
+using ContractMonthlyClaimSystem4.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ContractMonthlyClaimSystem4.Controllers
 {
-    public class HRController
+    [Authorize(Roles = "HR")]
+    public class HRController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HRController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HR/ApprovedClaims
+        public async Task<IActionResult> ApprovedClaims()
+        {
+            var approvedClaims = await _context.Claims
+                .Include(c => c.SubmitterId)
+                .Where(c => c.Status == ClaimStatus.Approved)
+                .ToListAsync();
+
+            return View(approvedClaims);
+        }
+
+        // Additional actions will be added later
     }
 }
