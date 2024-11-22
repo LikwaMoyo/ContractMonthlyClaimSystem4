a1577e4c3e28b35c1b929d8f8eedeb3141e50451
ST10132139 <102595139+ST10132139@users.noreply.github.com>
2024-11-22 13:06:36 +0200

    Lecturer automation works, added claimSubmission.js

--- Controllers/HRController.cs
+++ Controllers/HRController.cs
@@ -11,6 +11,8 @@ namespace ContractMonthlyClaimSystem4.Controllers
     [Authorize(Roles = "HR")]
     public class HRController : Controller
     {
+
+        //
         private readonly ApplicationDbContext _context;
 
         public HRController(ApplicationDbContext context)
